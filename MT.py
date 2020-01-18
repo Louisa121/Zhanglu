465ac50f28f5949d5b13060d99a61c4977006419
Louisa121 <36052601+Louisa121@users.noreply.github.com>
2020-01-18 12:23:11 +0200

    Update MT.py

--- MT.py
+++ MT.py
@@ -1,7 +1,7 @@
 #### Set second to 00 ####
 
 import pandas as pd
-
+last30 = pd.datetime.now().replace(microsecond=0) - pd.Timedelta('30H')
 s = pd.date_range(last30, periods = 30 * 60 * 60, freq='S')
 s1=pd.DataFrame(s)
 s1[0].apply(lambda x: x.replace(second=0))
